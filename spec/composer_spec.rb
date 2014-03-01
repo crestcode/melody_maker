6dab0f1e5b48b82dc4287c76db3e130b7f37661d
John Kasiewicz <crestcode@gmail.com>
2014-03-01 10:11:26 -0500

    add tests for composer class

---
+++ spec/composer_spec.rb
@@ -0,0 +1,34 @@
+require "spec_helper"
+
+
+module MelodyMaker
+  describe Composer do
+    describe "#compose" do
+
+      it "returns an array" do
+        Composer.new.compose.should be_a(Array)
+      end
+
+      it "returns an array with 7 elements by default" do
+        expect(Composer.new.compose.size).to eq(7)
+      end
+
+      it "returns an array with X elements when X is passed in" do
+        expect(Composer.new.compose(1).size).to eq(1)
+        expect(Composer.new.compose(11).size).to eq(11)
+      end
+
+      it "each note is an integer" do
+        Composer.new.compose.each do |note|
+          expect(note).to be_a(Integer)
+        end
+      end
+
+      it "each note is a 15 or less" do
+        Composer.new.compose.each do |note|
+          expect(note).to be <= 15
+        end
+      end
+    end
+  end
+end
