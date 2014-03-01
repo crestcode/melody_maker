c23650da3a5b58f9e0a505edfb9044499734277d
John Kasiewicz <crestcode@gmail.com>
2014-03-01 09:54:20 -0500

    add composer class

---
+++ lib/melody_maker/composer.rb
@@ -0,0 +1,14 @@
+module MelodyMaker
+  class Composer
+
+    def compose(melody_length = 7)
+      Array.new(melody_length) { random_pitch }
+    end
+
+    private
+
+    def random_pitch
+      rand(1...15)
+    end
+  end
+end
